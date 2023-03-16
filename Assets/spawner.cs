d934d8936de192276ebf124a9c2a791f7148eb0f
Ander Hernández <ander.hernandez@opendeusto.es>
2023-03-16 09:24:56 +0100

    Disparo Personaje
    Creación de script de disparo del personaje

--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
+    public GameObject bullet;
+    public Vector3 position;
+    public Vector3 aiming;
+    public float coolDown ;
+    [SerializeField]
+    private float force = 1000;
     // Start is called before the first frame update
     void Start()
     {
-        
+        aiming = new Vector3(0, 0, 1);
+        Debug.LogWarning(aiming);
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        if (coolDown > 0) {
+
+            coolDown -= Time.deltaTime;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+           
+            if (coolDown <= 0) {
+                coolDown = 1;
+                position = this.transform.position;
+               GameObject newBullet = Instantiate(bullet);
+                newBullet.transform.position = this.transform.position + aiming + (Vector3.up*0.7f) ;
+                newBullet.GetComponent<Rigidbody>().AddForce(force * aiming);
+                Debug.Log(aiming);
+                
+
+
+                
+        }
+        
+        }
+
         
     }
 }
